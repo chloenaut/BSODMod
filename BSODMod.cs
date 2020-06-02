5b7841b81ccff54a2247c53f03ff8230d1554309
Chlo <frogminpepe@gmail.com>
2020-06-02 18:21:11 -0400

    Add mod content

---
+++ BSODMod.cs
@@ -0,0 +1,62 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.UI;
+
+namespace BSODMod
+{
+	public class BsodMod : Mod
+	{
+		private Texture2D BSODTEX;
+		public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+		{
+			int inventoryIndex = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Interface Logic 4"));
+			if (inventoryIndex != -1)
+			{
+				layers.Insert(inventoryIndex, new LegacyGameInterfaceLayer(
+					"BSOD: CrashScreen",
+					delegate
+					{
+						if (Main.LocalPlayer.dead)
+						{
+							Color color = Color.Blue;
+							Texture2D tex = this.BSODTEX;
+							Rectangle sourceRectangle = new Rectangle(
+								x: 0,
+								y: 0,
+								width: tex.Width,
+								height: tex.Height
+							);
+							
+							Main.spriteBatch.Draw(
+								texture: tex,
+								destinationRectangle: new Rectangle(-2, -2, Main.screenWidth + 4, Main.screenHeight + 4),
+								sourceRectangle: sourceRectangle,
+								color: Color.White,
+								rotation: default,
+								origin: default,
+								effects: SpriteEffects.None,
+								layerDepth: 1f
+							);
+						}
+
+						return true;
+					},
+					InterfaceScaleType.Game)
+				);
+			}
+
+			base.ModifyInterfaceLayers(layers);
+		}
+
+		public override void PostSetupContent()
+		{
+			if (!Main.dedServ && Main.netMode != 2)
+			{
+				this.BSODTEX = this.GetTexture("fakebsod");
+			}
+		}
+	}
+}
---
+++ MyPlayer.cs
@@ -0,0 +1,23 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace BSODMod
+{
+	class MyPlayer : ModPlayer
+	{
+		private int deathTimer = 180;
+		public override void PreUpdate()
+		{
+			if (player.dead)
+			{
+				deathTimer -= 1;
+				if (deathTimer == 0)
+				{
+					Main.instance.Exit();
+				}
+			}
+			base.PreUpdate();
+		}
+
+	}
+}
